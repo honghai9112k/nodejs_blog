4fa5dc855948b4da0f3e3c3aa4346844c0737f2d
Hong Hai Nguyen <honghainguyen2000@gmail.com>
2021-08-19 21:23:02 +0700

    Model Mogoose DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,17 @@
-
+const Course = require('../models/Course');
 class SiteController {
-    
     // [GET] /
     index(req, res) {
-        res.render('home')
+        Course.find({}, function (err, courses) {
+            if (!err) res.json(courses);
+            else res.status(400).json({ error: 'ERROR!!! ' });
+        });
     }
 
     // [GET] /search
     search(req, res) {
-        res.render('search')
+        res.render('search');
     }
 }
 
-module.exports = new SiteController
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
+const db = require('./config/db');
 
-app.use(express.static(path.join(__dirname, 'public')))
+// Connect to DB
+db.connect();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', handlebars({
-    extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-route(app)
+route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
