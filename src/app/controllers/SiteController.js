3379a8e4bde8653e6879713271b581870e2da5d8
Hong Hai Nguyen <honghainguyen2000@gmail.com>
2021-08-20 16:18:10 +0700

    CRUD Read from Db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,11 +1,24 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     // [GET] /
-    index(req, res) {
-        Course.find({}, function (err, courses) {
-            if (!err) res.json(courses);
-            else res.status(400).json({ error: 'ERROR!!! ' });
-        });
+    index(req, res, next) {
+        // cach 1 dung callback
+        // Course.find({}, function (err, courses) {
+        //     if (!err) res.json(courses);
+        //     else{
+        //         next(err)
+        //     }
+        // })
+
+        // cach 2 dung promisses
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
 
     // [GET] /search
--- src/index.js
+++ src/index.js
@@ -24,10 +24,10 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
